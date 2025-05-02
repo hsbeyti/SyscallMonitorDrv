8bf0ae1253b216fc5cf54665ba566d86ba7c2830
Hassan Sbeyti <hsbeity@hmail.com>
2025-05-02 08:02:53 +0200

    add unload to driver.c to stop driver

--- src/Driver.c
+++ src/Driver.c
@@ -1,110 +1,121 @@
-﻿// src/Driver.c
-
-#include <ntddk.h>
+﻿#include <ntddk.h>
 #include <wdf.h>
+
 #include "../inc/Communication.h"
 #include "../inc/HypervisorSetup.h"
 #include "../inc/ProcessNotification.h"
 #include "../inc/SyscallFilter.h"
 
-DRIVER_INITIALIZE        DriverEntry;
+DRIVER_INITIALIZE DriverEntry;
 EVT_WDF_DRIVER_DEVICE_ADD EvtDeviceAdd;
-EVT_WDF_DRIVER_UNLOAD     EvtDriverUnload;
+EVT_WDF_DRIVER_UNLOAD EvtDriverUnload;
+
+#define LOG_RAW(msg) \
+    DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[SyscallMonitorDrv] %s\n", msg)
+
+#define LOG(fmt, ...) \
+    DbgPrintEx(DPFLTR_IHVDRIVER_ID, DPFLTR_INFO_LEVEL, "[SyscallMonitorDrv] " fmt "\n", __VA_ARGS__)
+
+// Global device handle for cleanup if needed
+static WDFDEVICE g_Device = NULL;
 
 NTSTATUS
 DriverEntry(
-    _In_ PDRIVER_OBJECT   DriverObject,
-    _In_ PUNICODE_STRING  RegistryPath
+    _In_ PDRIVER_OBJECT DriverObject,
+    _In_ PUNICODE_STRING RegistryPath
 )
 {
     WDF_DRIVER_CONFIG config;
     WDF_DRIVER_CONFIG_INIT(&config, EvtDeviceAdd);
     config.EvtDriverUnload = EvtDriverUnload;
 
-    return WdfDriverCreate(
+    LOG_RAW("DriverEntry called");
+
+    NTSTATUS status = WdfDriverCreate(
         DriverObject,
         RegistryPath,
         WDF_NO_OBJECT_ATTRIBUTES,
         &config,
         WDF_NO_HANDLE
     );
+
+    if (!NT_SUCCESS(status)) {
+        LOG("WdfDriverCreate failed: 0x%X", status);
+    }
+    else {
+        LOG_RAW("WdfDriverCreate succeeded");
+    }
+
+    return status;
 }
 
 NTSTATUS
 EvtDeviceAdd(
-    _In_    WDFDRIVER        Driver,
-    _Inout_ PWDFDEVICE_INIT  DeviceInit
+    _In_ WDFDRIVER Driver,
+    _Inout_ PWDFDEVICE_INIT DeviceInit
 )
 {
     UNREFERENCED_PARAMETER(Driver);
+    LOG_RAW("EvtDeviceAdd called");
+
     NTSTATUS status;
 
-    //
-    // 1) Give the device an NT name: "\Device\SyscallMonitor"
-    //
     static const UNICODE_STRING ntName = RTL_CONSTANT_STRING(L"\\Device\\SyscallMonitor");
+    static const UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\DosDevices\\SyscallMonitor");
+
     status = WdfDeviceInitAssignName(DeviceInit, &ntName);
     if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: WdfDeviceInitAssignName failed 0x%X\n", status));
+        LOG("WdfDeviceInitAssignName failed: 0x%X", status);
         return status;
     }
 
-    //
-    // 2) Create the WDFDEVICE
-    //
     WDF_OBJECT_ATTRIBUTES attrs;
     WDF_OBJECT_ATTRIBUTES_INIT(&attrs);
 
-    WDFDEVICE device;
-    status = WdfDeviceCreate(&DeviceInit, &attrs, &device);
+    status = WdfDeviceCreate(&DeviceInit, &attrs, &g_Device);
     if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: WdfDeviceCreate failed 0x%X\n", status));
+        LOG("WdfDeviceCreate failed: 0x%X", status);
         return status;
     }
 
-    //
-    // 3) Create the symbolic link "\DosDevices\SyscallMonitor"
-    //
-    static const UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\DosDevices\\SyscallMonitor");
-    status = WdfDeviceCreateSymbolicLink(device, &symLink);
+    status = WdfDeviceCreateSymbolicLink(g_Device, &symLink);
     if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: WdfDeviceCreateSymbolicLink failed 0x%X\n", status));
+        LOG("WdfDeviceCreateSymbolicLink failed: 0x%X", status);
         return status;
     }
 
-    //
-    // 4) Now you can set up IOCTL queue, hypervisor, etc.
-    //
-    status = RegisterIoctlHandlers(device);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: RegisterIoctlHandlers failed 0x%X\n", status));
+    // Step-by-step initialization
+    if (!NT_SUCCESS(status = RegisterIoctlHandlers(g_Device))) {
+        LOG("RegisterIoctlHandlers failed: 0x%X", status);
         return status;
     }
+    LOG_RAW("IOCTL handlers registered");
 
-    status = SetupCommunication(device);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: SetupCommunication failed 0x%X\n", status));
+    if (!NT_SUCCESS(status = SetupCommunication(g_Device))) {
+        LOG("SetupCommunication failed: 0x%X", status);
         return status;
     }
+    LOG_RAW("Communication setup complete");
 
-    status = InitializeHypervisor();
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: InitializeHypervisor failed 0x%X\n", status));
+    if (!NT_SUCCESS(status = InitializeHypervisor())) {
+        LOG("InitializeHypervisor failed: 0x%X", status);
         return status;
     }
+    LOG_RAW("Hypervisor initialized");
 
-    status = RegisterProcessNotifications();
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: RegisterProcessNotifications failed 0x%X\n", status));
+    if (!NT_SUCCESS(status = RegisterProcessNotifications())) {
+        LOG("RegisterProcessNotifications failed: 0x%X", status);
         return status;
     }
+    LOG_RAW("Process notification registered");
 
-    status = InitializeSyscallFilter();
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: InitializeSyscallFilter failed 0x%X\n", status));
+    if (!NT_SUCCESS(status = InitializeSyscallFilter())) {
+        LOG("InitializeSyscallFilter failed: 0x%X", status);
         return status;
     }
+    LOG_RAW("Syscall filter initialized");
 
+    LOG_RAW("Device successfully added");
     return STATUS_SUCCESS;
 }
 
@@ -114,131 +125,19 @@ EvtDriverUnload(
 )
 {
     UNREFERENCED_PARAMETER(Driver);
+    LOG_RAW("EvtDriverUnload called");
 
     CleanupSyscallFilter();
+    LOG_RAW("Syscall filter cleaned");
+
     UnregisterProcessNotifications();
+    LOG_RAW("Process notifications unregistered");
+
     ShutdownHypervisor();
+    LOG_RAW("Hypervisor shutdown");
+
     CleanupCommunication();
-}
-// src/Driver.c
+    LOG_RAW("Communication cleaned");
 
-#include <ntddk.h>
-#include <wdf.h>
-#include "../inc/Communication.h"
-#include "../inc/HypervisorSetup.h"
-#include "../inc/ProcessNotification.h"
-#include "../inc/SyscallFilter.h"
-
-DRIVER_INITIALIZE        DriverEntry;
-EVT_WDF_DRIVER_DEVICE_ADD EvtDeviceAdd;
-EVT_WDF_DRIVER_UNLOAD     EvtDriverUnload;
-
-NTSTATUS
-DriverEntry(
-    _In_ PDRIVER_OBJECT   DriverObject,
-    _In_ PUNICODE_STRING  RegistryPath
-)
-{
-    WDF_DRIVER_CONFIG config;
-    WDF_DRIVER_CONFIG_INIT(&config, EvtDeviceAdd);
-    config.EvtDriverUnload = EvtDriverUnload;
-
-    return WdfDriverCreate(
-        DriverObject,
-        RegistryPath,
-        WDF_NO_OBJECT_ATTRIBUTES,
-        &config,
-        WDF_NO_HANDLE
-    );
-}
-
-NTSTATUS
-EvtDeviceAdd(
-    _In_    WDFDRIVER        Driver,
-    _Inout_ PWDFDEVICE_INIT  DeviceInit
-)
-{
-    UNREFERENCED_PARAMETER(Driver);
-    NTSTATUS status;
-
-    //
-    // 1) Give the device an NT name: "\Device\SyscallMonitor"
-    //
-    static const UNICODE_STRING ntName = RTL_CONSTANT_STRING(L"\\Device\\SyscallMonitor");
-    status = WdfDeviceInitAssignName(DeviceInit, &ntName);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: WdfDeviceInitAssignName failed 0x%X\n", status));
-        return status;
-    }
-
-    //
-    // 2) Create the WDFDEVICE
-    //
-    WDF_OBJECT_ATTRIBUTES attrs;
-    WDF_OBJECT_ATTRIBUTES_INIT(&attrs);
-
-    WDFDEVICE device;
-    status = WdfDeviceCreate(&DeviceInit, &attrs, &device);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: WdfDeviceCreate failed 0x%X\n", status));
-        return status;
-    }
-
-    //
-    // 3) Create the symbolic link "\DosDevices\SyscallMonitor"
-    //
-    static const UNICODE_STRING symLink = RTL_CONSTANT_STRING(L"\\DosDevices\\SyscallMonitor");
-    status = WdfDeviceCreateSymbolicLink(device, &symLink);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: WdfDeviceCreateSymbolicLink failed 0x%X\n", status));
-        return status;
-    }
-
-    //
-    // 4) Now you can set up IOCTL queue, hypervisor, etc.
-    //
-    status = RegisterIoctlHandlers(device);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: RegisterIoctlHandlers failed 0x%X\n", status));
-        return status;
-    }
-
-    status = SetupCommunication(device);
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: SetupCommunication failed 0x%X\n", status));
-        return status;
-    }
-
-    status = InitializeHypervisor();
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: InitializeHypervisor failed 0x%X\n", status));
-        return status;
-    }
-
-    status = RegisterProcessNotifications();
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: RegisterProcessNotifications failed 0x%X\n", status));
-        return status;
-    }
-
-    status = InitializeSyscallFilter();
-    if (!NT_SUCCESS(status)) {
-        KdPrint(("Driver: InitializeSyscallFilter failed 0x%X\n", status));
-        return status;
-    }
-
-    return STATUS_SUCCESS;
-}
-
-VOID
-EvtDriverUnload(
-    _In_ WDFDRIVER Driver
-)
-{
-    UNREFERENCED_PARAMETER(Driver);
-
-    CleanupSyscallFilter();
-    UnregisterProcessNotifications();
-    ShutdownHypervisor();
-    CleanupCommunication();
+    LOG_RAW("Driver unload completed successfully");
 }
